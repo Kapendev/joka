fe8d1ae42062679f7a307ea43535c92ba22bda05
Kapendev <alexandroskapretsos@gmail.com>
2025-07-17 15:09:19 +0300

    VERY IMPORTANT ACTUALLY

--- examples/_002_math.d
+++ examples/_002_math.d
@@ -7,13 +7,14 @@ void main() {
     auto a = IVec2(2, 4);
     auto b = IVec3(a * a, 8);
     auto c = IVec4(b + b, 16);
-    trace(c);
+    println(c);
 
     // Loop over the components of a vector.
-    foreach (ref item; c) item *= 2;
-    trace(c);
-    foreach (i; 0 .. c.length) c[i] *= 2;
-    trace(c);
+    auto e = IVec4(2, 3, 4, 5);
+    e[][] *= 2;
+    foreach (ref item; e) item *= 2;
+    foreach (i; 0 .. e.length) e[i] *= 2;
+    println(e);
 
     // Create a rectangle and change its position and size.
     auto rect = IRect(a, 30, 32);
@@ -21,5 +22,5 @@ void main() {
     rect.x += 1;
     rect.size.x += 1;
     rect.w += 1;
-    trace(rect);
+    println(rect);
 }
