5a9bbd9ad00d11c974c284fe148d374a4bfa4d54
Kapendev <alexandroskapretsos@gmail.com>
2026-06-26 23:53:13 +0300

    Changed how trace and todo work.

--- examples/_002_math.d
+++ examples/_002_math.d
@@ -8,7 +8,7 @@ void main() {
     auto a2 = IVec3(a1 * a1, 8);
     auto a3 = IVec4(a2 + a2, 16);
     auto a4 = a3.chop().chop();
-    trace(a1, a2, a3, a4);
+    debugPrint(a1, a2, a3, a4);
 
     // Loop over the components of a vector.
     auto b = IVec4(2, 3, 4, 5);
@@ -16,15 +16,15 @@ void main() {
     b[][] += 2;
     foreach (ref item; b) item += 2;
     foreach (i; 0 .. b.length) b[i] += 2;
-    trace(b);
+    debugPrint(b);
 
     // Swizzle operations.
     auto c = IVec3(2, 0, 4);
-    trace(c.swizzle(2, 0, 1), c.swizzle("zxy"), c.yx);
+    debugPrint(c.swizzle(2, 0, 1), c.swizzle("zxy"), c.yx);
 
     // Create a rectangle and change its position and size.
     auto rect = IRect(a1, 30, 32);
     rect.position += IVec2(1);
     rect.size += IVec2(1);
-    trace(rect.x, rect.y, rect.w, rect.h);
+    debugPrint(rect.x, rect.y, rect.w, rect.h);
 }
